a2f76f792240516334e14fc3467841119297a430
Aleksandr M <43361790+aleksandrmaiorov@users.noreply.github.com>
2022-08-04 00:49:59 -0400

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sonuc = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in sonuc:
+                return [sonuc[diff], i]
+            sonuc[n] = i
