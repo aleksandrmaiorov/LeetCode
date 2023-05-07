f809095b6daf26fa97e9fee03685a14d32984e6b
aleksandrmaiorov <aleksandr@aleksandr.at>
2023-05-07 15:51:29 -0400

    added new line to roman_to_integer

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -1,4 +1,6 @@
 ## MY SOLUTION
+##Feel free to use this code.
+
 class Solution:
     def romanToInt(self, s: str) -> int:
         romali ={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
