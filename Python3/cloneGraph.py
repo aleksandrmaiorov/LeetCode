d1a8500a832e296f29bb78d3f06d20da4a274349
aleksandrmaiorov <aleksandr@aleksandr.at>
2023-05-10 04:56:05 -0400

    Added new solutions

---
+++ Python3/cloneGraph.py
@@ -0,0 +1,19 @@
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        def clone_graph(node, indexed = dict()):
+            if(not node):
+                return None
+            
+            if(node.val in indexed):
+                return indexed[node.val]
+            
+            indexed[node.val] = Node(node.val)
+            for nei in node.neighbors:
+                indexed[node.val]\
+                .neighbors\
+                .append(clone_graph(nei, indexed))
+            
+            return indexed[node.val]
+        
+        return clone_graph(node)
